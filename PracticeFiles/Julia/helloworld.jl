1f60620fe056299b2193c900ae078f110cff8ce5
Timothy Adcock <Timothy.Adcock@mines.sdsmt.edu>
2020-09-04 18:23:06 -0600

    Added Starter files to timTEST
    Just for testing

---
+++ PracticeFiles/Julia/helloworld.jl
@@ -0,0 +1,24 @@
+
+fname = "Project\\TALA\\juliaTEST\\test.txt"
+
+open(fname, "r") do f
+    for line in eachline(f)
+        println(line)
+    end
+end
+
+
+f = open(fname, "r")
+show(readlines(f));
+println();
+close(f)
+
+print("What is your Name: ")
+
+n = readline()
+
+print("What is your profession: ")
+
+p = readline()
+
+println("You are $n with a profession of $p")
